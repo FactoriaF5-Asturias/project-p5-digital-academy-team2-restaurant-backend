f312362b7e07ad337f45e6c66b877e4a403f2a6e
Juan <juan.isidro.menendez@gmail.com>
2026-09-15 12:49:02 +0200

    feat: UserEntity.java created

---
+++ src/main/java/factoriaf5/team2/goxu/users/UserEntity.java
@@ -0,0 +1,48 @@
+package factoriaf5.team2.goxu.users;
+
+import java.util.Set;
+
+import factoriaf5.team2.goxu.roles.RoleEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "roles_users", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    Set<RoleEntity> roles;
+
+} UserEntity {
+    
+}
