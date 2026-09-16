9cf33548988211573fb0742c860cb381c96ac5a5
Juan <juan.isidro.menendez@gmail.com>
2026-09-16 14:05:01 +0200

    refactor: type RoleEntity name field with RoleName enum

--- src/main/java/factoriaf5/team2/goxu/roles/RoleEntity.java
+++ src/main/java/factoriaf5/team2/goxu/roles/RoleEntity.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.ManyToMany;
@@ -25,7 +27,8 @@ public class RoleEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_role")
     private Long id;
-    private String name;
+    @Enumerated(EnumType.STRING)
+    private RoleName name;     /* Sustituyo por un RoleName */
 
     @JsonIgnore
     @ManyToMany(mappedBy = "roles")
