899489b6cf056be277fdffcc5651f2a7bfad3f9c
Juan <juan.isidro.menendez@gmail.com>
2026-09-23 16:10:43 +0200

    feat: assign default customer role on RegisterService.java

--- src/main/java/factoriaf5/team2/goxu/register/RegisterService.java
+++ src/main/java/factoriaf5/team2/goxu/register/RegisterService.java
@@ -2,6 +2,7 @@ package factoriaf5.team2.goxu.register;
 
 import factoriaf5.team2.goxu.register.dtos.RegisterDTORequest;
 import factoriaf5.team2.goxu.register.dtos.RegisterDTOResponse;
+import factoriaf5.team2.goxu.roles.RoleService;
 import factoriaf5.team2.goxu.users.UserEntity;
 import factoriaf5.team2.goxu.users.UserRepository;
 
@@ -10,19 +11,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/* Falta para ser operativo: 
-decrypt, roleservice*/
-
 @Service
 public class RegisterService {
 
-    //Inyección del repo y el encoder.
+    //Inyección del repo y el encoder y el servicio de roles.
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleService roleService;
     
-    public RegisterService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public RegisterService(UserRepository userRepository, PasswordEncoder passwordEncoder, 
+        RoleService roleService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.roleService = roleService;
     }
     @Transactional
     public RegisterDTOResponse registerUser(RegisterDTORequest dto) {
@@ -39,7 +40,10 @@ public class RegisterService {
         UserEntity userToSave = RegisterMapper.toEntity(dto);
         userToSave.setPassword(passwordEncoder.encode(dto.password()));
 
-        // 3. Guarda y emite DTO de respuesta
+        // 3. Asigna el rol por defecto (customer)
+        userToSave.setRoles(roleService.assignDefaultRole());
+        
+        // 4. Guarda y emite DTO de respuesta
         UserEntity saved = userRepository.save(userToSave);
         return RegisterMapper.toDTO(saved);
     
